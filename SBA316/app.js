bd29c2d2bb4f9b6396e760047c377d8c2f6f7181
Chanel Barnes <56006101+chanelbarnes@users.noreply.github.com>
2024-03-31 14:13:22 -0400

    Add files via upload

---
+++ SBA316/app.js
@@ -0,0 +1,51 @@
+const form = document.getElementById("form");
+
+const totalInput = form.elements["total"];
+const optionsSelect = form.elements["options"];
+const rightContainer = document.getElementById("rightContainer");
+
+
+
+form.addEventListener("submit", calculate);
+
+function calculate (event) {
+// prevent default submission
+    event.preventDefault();
+//get dropdown value
+    var selectedValue = optionsSelect.value;
+
+//get userInput total
+    var bill = parseFloat(totalInput.value);
+
+    let tip = selectedValue * bill;
+    let tip1 = tip / 100;
+
+//display tip amount    
+    alert (`Tip $${tip1}`);
+
+ //create paragraph node
+ var paragraph = document.createElement("p");
+
+ //create text node with tip amount
+ var tipTextNode = document.createTextNode(`Tipped: $${tip1}`);
+
+ //append text node to paragraph
+ paragraph.appendChild(tipTextNode);
+
+ //append paragraph to rightContainer
+ rightContainer.appendChild(paragraph);
+}
+
+const h1 = document.querySelectorAll("h1");
+
+h1.forEach(function(h1) {
+    h1.addEventListener("mouseenter", function() {
+
+    h1.style.backgroundColor = "red";
+})
+
+h1.addEventListener("mouseleave", function() {
+
+    h1.style.backgroundColor = "";
+});
+})
